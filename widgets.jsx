36aeada4c7840e45fd0ad6918228645921939036
Jonathon Wright <jonwright93@gmail.com>
2017-03-05 21:35:31 -0800

    initiate autocomplete widget

--- widgets.jsx
+++ widgets.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Tabs from './tabs';
 import Clock from './clock';
+import Autocomplete from './autocomplete';
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(<Root/>, document.getElementById('main'));
@@ -14,11 +15,23 @@ const panes  = [
   {title: 'fourth', content: 'fourth description'},
 ];
 
+const Names = [
+  'Jon',
+  'Jenn',
+  'Judy',
+  'Jessica',
+  'Bart',
+  'Remy',
+  'Sally',
+  'Xavier'
+];
+
 class Root extends React.Component {
   render() {
     return(
       <div>
         <Tabs panes={panes}/>
+        <Autocomplete names={Names}/>
         <Clock/>
       </div>
     );
